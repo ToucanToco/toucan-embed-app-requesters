38971bc8b47cdf9607a5efedcb36df566a13729b
Jordan Lorho <jordan.lorho@gmail.com>
2021-01-15 16:34:12 +0100

    style: beautiful integration baby

--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,23 @@
 <template>
   <div class="container">
-    <select v-model="selectedView">
-      <option v-for="(cat, index) in categories" :key="index" :value="cat">
-        {{ cat }}
-      </option>
-    </select>
-    <select v-model="selectedDate">
-      <option value="December 2017">December 2017</option>
-      <option value="October 2017">October 2017</option>
-    </select>
+    <div class="row filters-container">
+      <div class="filter-group">
+        <span class="filter-group__label">REPORT</span>
+        <select v-model="selectedView">
+          <option v-for="cat in categories" :key="cat" :value="cat">
+            {{ cat }}
+          </option>
+        </select>
+      </div>
+      <div class="filter-group">
+        <span class="filter-group__label">DATE</span>
+        <select v-model="selectedDate">
+          <option v-for="date in dates" :key="date" :value="date">
+            {{ date }}
+          </option>
+        </select>
+      </div>
+    </div>
     <div class="row">
       <!-- <script
         async
@@ -31,18 +40,18 @@
         type="text/javascript"
       ></script> -->
     </div>
-    <div class="row">
+    <div class="row stories-container">
       <div class="story-container">
         <script
           async
-          src="https://demo-embed.toucantoco.com/scripts/embedLauncher.js?id=25e3c3d2-589e-4229-9f9c-7ca034a3dc08&view=Tables"
+          src="https://demo-embed.toucantoco.com/scripts/embedLauncher.js?id=25e3c3d2-589e-4229-9f9c-7ca034a3dc08&view=Tables&title=true"
           type="text/javascript"
         ></script>
       </div>
       <div class="story-container">
         <script
           async
-          src="https://demo-embed.toucantoco.com/scripts/embedLauncher.js?id=c8126df7-b6a7-4f82-a391-8f6f7c027f12&view=Tables"
+          src="https://demo-embed.toucantoco.com/scripts/embedLauncher.js?id=c8126df7-b6a7-4f82-a391-8f6f7c027f12&view=Tables&title=true"
           type="text/javascript"
         ></script>
       </div>
@@ -74,6 +83,20 @@ export default {
         'Furnishings',
         'Copiers',
       ],
+      dates: [
+        'December 2017',
+        'November 2017',
+        'October 2017',
+        'September 2017',
+        'August 2017',
+        'July 2017',
+        'June 2017',
+        'May 2017',
+        'April 2017',
+        'March 2017',
+        'February 2017',
+        'January 2017',
+      ],
       selectedView: 'Tables',
       selectedDate: 'December 2017',
       toucanSDK: null,
@@ -102,12 +125,10 @@ export default {
 <style>
 .container {
   margin: 0 auto;
+  padding: 20px;
   min-height: 100vh;
   display: flex;
   flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
 }
 
 .row {
@@ -115,9 +136,47 @@ export default {
   width: 100%;
 }
 
+.stories-container {
+  justify-content: space-between;
+}
+
 .story-container {
   display: flex;
   height: 80vh;
-  width: 100%;
+  width: 49%;
+  border: 1px solid #cecece;
+  border-radius: 2px;
+  -webkit-box-shadow: 0 0 8px 0 rgba(0, 0, 0, 0.08);
+  box-shadow: 0 0 8px 0 rgba(0, 0, 0, 0.08);
+}
+
+.filters-container {
+  display: flex;
+  border: 1px solid #cecece;
+  margin-bottom: 20px;
+  height: 60px;
+}
+
+.filter-group {
+  display: flex;
+  flex-direction: column;
+  color: #888;
+  padding: 10px;
+}
+
+.filter-group__label {
+  /* display: flex;
+  justify-content: start; */
+  font-size: 11px;
+  margin-left: 5px;
+}
+
+.filter-group select {
+  border: none;
+  font-weight: bold;
+  color: #888;
+  outline: 0;
+  margin-top: 5px;
+  font-size: 18px;
 }
 </style>
